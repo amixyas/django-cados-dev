6676ad8deaf1cebfe7c55c9a286241b732a711ba
amixyas <i.amirat@esi-sba.dz>
2022-11-20 10:36:37 +0100

    use Q model for queries

--- base/views.py
+++ base/views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Advocate
 from .serializers import AdvocateSerializer
-
+from django.db.models import Q
 
 
 @api_view(['GET'])
@@ -25,7 +25,7 @@ def advocate_list (request):
     print('Query: ',query)
     #http://127.0.0.1:8000/advocates/?query=y
     #advocates = Advocate.objects.all()
-    advocates = Advocate.objects.filter(username__icontains=query)
+    advocates = Advocate.objects.filter(Q(username__icontains=query) | Q(bio__icontains=query))
     serializer = AdvocateSerializer(advocates, many=True)
     return Response(serializer.data)
 
